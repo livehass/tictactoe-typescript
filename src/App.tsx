5456252edcd98590b0de0354b18f0d16e61be377
livehass <silva.felipe12@hotmail.com>
2024-04-23 22:30:40 -0300

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
 import logo from './logo.svg';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './App.css';
+import './Reset.css'
+import { title } from 'process';
+import App from './App';
 
-function App() {
+
+import React, { FC, useState } from 'react';
+
+interface SquareProps {
+  value: string | null;
+  onSquareClick: ()=> void;
+}
+
+const Square: FC<SquareProps> = ({ value , onSquareClick}) => {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+
+const Board: FC = () => {
+  const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null));
+  
+  const handleClick = (i: number): void => {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default Board;
